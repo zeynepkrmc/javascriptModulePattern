06c1c752a0c954f25577c1ed7bb2d8840da21ab6
Zeynep Sude <m.zeynephnm@gmail.com>
2023-09-28 15:04:02 +0300

    what is factorry pattern

---
+++ factoryPattern.js
@@ -0,0 +1,42 @@
+//A function which will create object.
+//Birbirine benzer objeleri üretecek yapı.
+
+function Factory(){
+
+    this.createEmployee = function(type){
+
+        var employee;
+        if(type === 'fulltime'){
+            employee = new FullTime();
+        }else if(type === 'parttime'){
+            employee = new PartTime();
+        }else if(type === 'temporary'){
+            employee = new Temporary();
+        }
+        employee.type = type;
+        employee.say = function(){
+            console.log(this.type + ' : price by hour : ' + this.hourly);
+        }
+        return employee;
+    }
+}
+
+var FullTime = function(){
+    this.hourly = '30TL';
+}
+var PartTime = function(){
+    this.hourly = '20TL';
+}
+var Temporary = function(){
+    this.hourly = '15TL';
+}
+
+var Factory = new Factory();
+
+
+
+var fulltimeEmp = Factory.createEmployee('fulltime');
+fulltimeEmp.say();
+
+var parttimeEmp = Factory.createEmployee('parttime');
+parttimeEmp.say();
