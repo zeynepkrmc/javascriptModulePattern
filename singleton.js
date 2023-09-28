413c3de6ade417dce3fc9af2852d88bc08ed8910
Zeynep Sude <m.zeynephnm@gmail.com>
2023-09-28 11:40:19 +0300

    output added as commit line

--- singleton.js
+++ singleton.js
@@ -1,8 +1,12 @@
 //singleton pattern
+//Singleton is a design pattern that tells us that we can create only one instance of a class and that instance can be accessed globally.
 var singleton = (function(){
 
     var instance;
     
+    //An object which is produced in different times, have the same referance. 
+    //We observe this from randomNumber, when we call getInstance if loop runs here:
+    //If there is an object created before, return the instance. If not create new instance.
     const createInstance =  function(){
         return {
             randomNumber : Math.random() //Randomly number will come as an object.
@@ -25,3 +29,4 @@ const i2 = singleton.getInstance();
 
 console.log(i1.randomNumber,
     i2.randomNumber);
+//output is 0.5371848672961654 0.5371848672961654
